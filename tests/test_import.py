c03db190d0139c60dec7f992eb4ae79fb5a33b01
Matthew Feickert <matthew.feickert@cern.ch>
2022-12-19 01:07:38 -0600

    Add Python import test

---
+++ tests/test_import.py
@@ -0,0 +1,9 @@
+import ROOT
+import numpy
+import lhapdf
+import pythia8
+import yoda
+import rivet
+
+# fastjet currently needs to be imported _after_ rivet to avoid ImportError due to Undefined Symbol
+import fastjet
